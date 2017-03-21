37b3189e248279d4fe0ef60ebe8c4f2adf724a18
zaytsev-dv <zaytsev.dimon@gmail.com>
2017-03-21 21:15:17 +0700

    update ViewServlet

--- src/main/java/controller/ViewUserServlet.java
+++ src/main/java/controller/ViewUserServlet.java
@@ -10,18 +10,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 import static factory.AbstractFactory.getFactory;
 
-@WebServlet("/viewUserServlet")
+@WebServlet("/view")
 public class ViewUserServlet extends HttpServlet {
     private AbstractFactory factory;
 
 
     public ViewUserServlet() {
-        factory = getFactory("hibernate");
+        try {
+            FileInputStream fileInputStream;
+            Properties properties=new Properties();
+            fileInputStream=new FileInputStream(
+                    "J:\\Java_Web_Projects\\Task_1\\src\\main\\resources\\factoryName.properties");
+            properties.load(fileInputStream);
+            String factoryName=properties.getProperty("factoryName");
+            factory = getFactory(factoryName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -35,10 +48,5 @@ public class ViewUserServlet extends HttpServlet {
         RequestDispatcher dispatcher = req.getRequestDispatcher("UserList.jsp");
         req.setAttribute("userList", newUserList);
         dispatcher.forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req,resp);
     }
 }
