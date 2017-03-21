978891a1bfa1b943ee40e47b59ae8707e305941c
zaytsev-dv <zaytsev.dimon@gmail.com>
2017-03-21 21:14:45 +0700

    update GetIdServlet

--- src/main/java/controller/GetIdUserServlet.java
+++ src/main/java/controller/GetIdUserServlet.java
@@ -10,11 +10,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 import static factory.AbstractFactory.getFactory;
 
-@WebServlet("/getIdUserServlet")
+@WebServlet("/getId")
 public class GetIdUserServlet extends HttpServlet {
 
 
@@ -22,14 +24,24 @@ public class GetIdUserServlet extends HttpServlet {
 
 
     public GetIdUserServlet() {
-        factory = getFactory("hibernate");
+        try {
+            FileInputStream fileInputStream;
+            Properties properties=new Properties();
+            fileInputStream=new FileInputStream(
+                    "J:\\Java_Web_Projects\\Task_1\\src\\main\\resources\\factoryName.properties");
+            properties.load(fileInputStream);
+            String factoryName=properties.getProperty("factoryName");
+            factory = getFactory(factoryName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-
-
         int id = Integer.parseInt(req.getParameter("id"));
         Service service=factory.getService();
         User newUser=null;
@@ -37,11 +49,6 @@ public class GetIdUserServlet extends HttpServlet {
         RequestDispatcher dispatcher = req.getRequestDispatcher("User.jsp");
         req.setAttribute("users_hib", newUser);
         dispatcher.forward(req, resp);
-
-    }
-    @Override
-    protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
     }
 }
 
