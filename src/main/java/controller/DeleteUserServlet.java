8020d47b05388671a148881b22f76d7f3b55a91e
zaytsev-dv <zaytsev.dimon@gmail.com>
2017-03-21 21:14:28 +0700

    update DeleteServlet

--- src/main/java/controller/DeleteUserServlet.java
+++ src/main/java/controller/DeleteUserServlet.java
@@ -9,35 +9,39 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 import static factory.AbstractFactory.getFactory;
 
-@WebServlet("/deleteUserServlet")
+@WebServlet("/delete")
 public class DeleteUserServlet extends HttpServlet {
     private AbstractFactory factory;
 
 
     public DeleteUserServlet() {
-        factory = getFactory("hibernate");
-    }
+        try {
+            FileInputStream fileInputStream;
+            Properties properties=new Properties();
+            fileInputStream=new FileInputStream(
+                    "J:\\Java_Web_Projects\\Task_1\\src\\main\\resources\\factoryName.properties");
+            properties.load(fileInputStream);
+            String factoryName=properties.getProperty("factoryName");
+            factory = getFactory(factoryName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         resp.setContentType("text/html");
         Service service=factory.getService();
         int id = Integer.parseInt(req.getParameter("id"));
-
         service.deleteUser(id);
-
-        RequestDispatcher dispatcher = req.getRequestDispatcher("viewUserServlet");
-
+        RequestDispatcher dispatcher = req.getRequestDispatcher("view");
         dispatcher.forward(req, resp);
-    }
-    @Override
-    protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
     }
 }
 
