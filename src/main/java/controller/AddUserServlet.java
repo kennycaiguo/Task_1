5c2ca2ff8ec0f7514af9ca8dc37a29c2fff2213e
zaytsev-dv <zaytsev.dimon@gmail.com>
2017-03-21 21:13:55 +0700

    update AddUserServlet

--- src/main/java/controller/AddUserServlet.java
+++ src/main/java/controller/AddUserServlet.java
@@ -10,40 +10,42 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Properties;
 
 import static factory.AbstractFactory.getFactory;
 
-@WebServlet("/addUserServlet")
+@WebServlet("/add")
 public class AddUserServlet extends HttpServlet {
 
     private AbstractFactory factory;
-    private String factoryName="hibernate";
+
 
     public AddUserServlet() {
-        factory = getFactory(factoryName);
+        try {
+            FileInputStream fileInputStream;
+            Properties properties=new Properties();
+            fileInputStream=new FileInputStream(
+                    "J:\\Java_Web_Projects\\Task_1\\src\\main\\resources\\factoryName.properties");
+            properties.load(fileInputStream);
+            String factoryName=properties.getProperty("factoryName");
+            factory = getFactory(factoryName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         Service service=factory.getService();
         service.addUser(name,email,password);
-
-        RequestDispatcher dispatcher = request.getRequestDispatcher("viewUserServlet");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("view");
         dispatcher.forward(request, response);
-
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req,resp);
     }
 }
 
