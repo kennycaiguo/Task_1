9a7f6f742843941cb298b9601800d91135d88395
zaytsev-dv <zaytsev.dimon@gmail.com>
2017-03-21 21:15:02 +0700

    update UpdateUserServlet

--- src/main/java/controller/UpdateUserServlet.java
+++ src/main/java/controller/UpdateUserServlet.java
@@ -9,40 +9,46 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 import static factory.AbstractFactory.getFactory;
 
-@WebServlet("/updateUserServlet")
+@WebServlet("/update")
 public class UpdateUserServlet extends HttpServlet{
 
     private AbstractFactory factory;
 
 
     public UpdateUserServlet() {
-        factory = getFactory("hibernate");
+        try {
+            FileInputStream fileInputStream;
+            Properties properties=new Properties();
+            fileInputStream=new FileInputStream(
+                    "J:\\Java_Web_Projects\\Task_1\\src\\main\\resources\\factoryName.properties");
+            properties.load(fileInputStream);
+            String factoryName=properties.getProperty("factoryName");
+            factory = getFactory(factoryName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-
         int id = Integer.parseInt(req.getParameter("id"));
         String name = req.getParameter("name");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         Service service=factory.getService();
         service.updateUser(id,name,email,password);
-
-
-        RequestDispatcher dispatcher = req.getRequestDispatcher("viewUserServlet");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("view");
         dispatcher.forward(req, resp);
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req,resp);
     }
 }
